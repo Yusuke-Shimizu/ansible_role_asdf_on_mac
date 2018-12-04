16bee18eb538a6e597f61eefe2dafcfe938e3c17
Yusuke Shimizu <shimi.nekoze@gmail.com>
2018-12-04 16:55:08 +0900

    add to install packages

--- spec/localhost/sample_spec.rb
+++ spec/localhost/sample_spec.rb
@@ -23,32 +23,49 @@ describe command("bash -lc 'asdf plugin-list'") do
 	its(:stdout) { should match /golang/ }
 end
 
+# check asdf lang
 describe command("bash -lc 'asdf current ruby'") do
 	its(:exit_status) { should eq 0 }
 	its(:stdout) { should match /2.5.3/ }
 end
-
 describe command("bash -lc 'asdf current python'") do
 	its(:exit_status) { should eq 0 }
 	its(:stdout) { should match /3.7.1/ }
 end
-
 describe command("bash -lc 'asdf current golang'") do
 	its(:exit_status) { should eq 0 }
 	its(:stdout) { should match /1.11/ }
 end
 
+# check lang version
 describe command("bash -lc 'ruby --version'") do
 	its(:exit_status) { should eq 0 }
 	its(:stdout) { should match /2.5.3/ }
 end
-
 describe command("bash -lc 'python --version'") do
 	its(:exit_status) { should eq 0 }
 	its(:stdout) { should match /3.7.1/ }
 end
-
 describe command("bash -lc 'go version'") do
 	its(:exit_status) { should eq 0 }
 	its(:stdout) { should match /1.11/ }
+end
+
+# 
+describe command("bash -lc 'gem --version'") do
+	its(:exit_status) { should eq 0 }
+	its(:stdout) { should match /2.5.3/ }
+end
+describe command("bash -lc 'gem list'") do
+	its(:exit_status) { should eq 0 }
+	its(:stdout) { should match /travis/ }
+end
+
+describe command("bash -lc 'pip --version'") do
+	its(:exit_status) { should eq 0 }
+	its(:stdout) { should match /travis/ }
+end
+describe command("bash -lc 'pip list'") do
+	its(:exit_status) { should eq 0 }
+	its(:stdout) { should match /awscli/ }
 end
