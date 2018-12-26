b1bba0b104cd894cd46a451d079e7930391d02b7
Yusuke Shimizu <shimi.nekoze@gmail.com>
2018-12-26 14:34:08 +0900

    add gem and pip test

--- spec/asdf_spec.rb
+++ spec/asdf_spec.rb
@@ -11,10 +11,10 @@ RSpec.shared_context 'check_command' do
 	its('exit_status') { should eq 0 }
 end
 
+# check asdf command
 describe command("bash -lc 'asdf --version'") do
 	include_context 'check_command'
 end
-
 describe command("bash -lc 'asdf plugin-list'") do
 	include_context 'check_command'
 	its('stdout') { should match /ruby/ }
@@ -22,6 +22,16 @@ describe command("bash -lc 'asdf plugin-list'") do
 	its('stdout') { should match /golang/ }
 end
 
+# check listed of package
+asdf_package_path = "#{ENV['HOME']}/.asdf/shims"
+describe gem('travis', "#{asdf_package_path}/gem") do
+  it { should be_installed }
+end
+describe pip('aws', "#{asdf_package_path}/pip") do
+  it { should be_installed }
+end
+
+# check to exit command
 commands = ["travis", "aws"]
 commands.each{|command|
 	describe command("bash -lc '#{command} --version'") do
